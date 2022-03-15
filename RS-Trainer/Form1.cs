861f80a18e99472845414858429e91bdc5699525
SyberiaImperor <Al.B.danilenko@gmail.com>
2022-03-15 21:15:42 +0300

    password screen

--- RS-Trainer/Form1.cs
+++ RS-Trainer/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,13 +17,35 @@ namespace RS_Trainer
         int currentVariant = 0;
         List<List<String>> variants;
 
-        void setText()
+        int inputPtr = 0;
+        String currentPassword;
+
+        void setTextStage1()
         {
             var lines = textBox1.Lines;
             lines[0] = ">" + variants[currentStage][currentVariant] + "<";
             lines[1] = variants[currentStage][(currentVariant + 1) % variants[currentStage].Count];
             textBox1.Clear();
             textBox1.Lines = lines;
+        }
+
+        void setText(String txt)
+        {
+            textBox1.Clear();
+            textBox1.Text = txt;
+        }
+
+        void nextInputPassword(Char nxt)
+        {
+            String line1 = textBox1.Lines[0];
+            StringBuilder txt = new StringBuilder(textBox1.Lines[1]);
+            if (inputPtr < txt.Length)
+            {
+                currentPassword += nxt;
+                txt[inputPtr] = '*';
+                inputPtr += 2;
+            }
+            textBox1.Lines = new string[] { line1, txt.ToString() };
         }
 
         public Form1()
@@ -33,7 +56,8 @@ namespace RS_Trainer
             variants[0].Add("Работа");
             variants[0].Add("Настройки");
             variants[0].Add("Тест");
-            setText();
+            variants.Add(new List<String>());
+            setTextStage1();
         }
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
@@ -48,9 +72,132 @@ namespace RS_Trainer
 
         private void button3_Click(object sender, EventArgs e)
         {
-            currentVariant += 1;
-            currentVariant %= variants[0].Count;
-            setText();
+            if (currentStage == 0)
+            {
+                currentVariant += 1;
+                currentVariant %= variants[0].Count;
+                setTextStage1();
+            }
+            
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            if (currentStage == 0)
+            {
+                setText("Доступ закрыт");
+                Thread.Sleep(1000);
+                setText("Введите пароль");
+                Thread.Sleep(1000);
+                String[] lines = new string[2];
+                lines[0] = "Пароль";
+                lines[1] = "_ _ _ _ _ _ _ _";
+                textBox1.Clear();
+                textBox1.Lines = lines;
+
+                currentStage += 1;
+            }
+            if (currentStage == 1)
+            {
+                if (currentPassword == "11111111")
+                {
+                    currentStage += 1;
+                    variants.Add(new List<String>());
+                    variants[currentStage].Add("Выдача");
+                    variants[currentStage].Add("Изменить");
+                    setTextStage1();
+                }
+                else
+                {
+                    inputPtr = 0;
+                    currentPassword = "";
+                    String[] lines = new string[2];
+                    lines[0] = "Пароль";
+                    lines[1] = "_ _ _ _ _ _ _ _";
+                    textBox1.Clear();
+                    textBox1.Lines = lines;
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (currentStage == 1)
+            {
+                nextInputPassword('1');
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (currentStage == 1)
+            {
+                nextInputPassword('2');
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (currentStage == 1)
+            {
+                nextInputPassword('3');
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (currentStage == 1)
+            {
+                nextInputPassword('4');
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (currentStage == 1)
+            {
+                nextInputPassword('5');
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (currentStage == 1)
+            {
+                nextInputPassword('6');
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (currentStage == 1)
+            {
+                nextInputPassword('7');
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (currentStage == 1)
+            {
+                nextInputPassword('8');
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (currentStage == 1)
+            {
+                nextInputPassword('9');
+            }
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if (currentStage == 1)
+            {
+                nextInputPassword('0');
+            }
         }
     }
 }
