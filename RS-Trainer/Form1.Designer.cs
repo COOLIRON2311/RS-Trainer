1bab8946f3d1b497dd6eb1b7a8cc3f7df16761fb
Anoth <mixaetel@gmail.com>
2022-03-22 17:43:38 +0300

    Поменял способ отображения информации на экране

--- RS-Trainer/Form1.Designer.cs
+++ RS-Trainer/Form1.Designer.cs
@@ -33,7 +33,6 @@ namespace RS_Trainer
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -51,26 +50,12 @@ namespace RS_Trainer
             this.button15 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
-            // 
-            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBox1.BackColor = System.Drawing.Color.DarkOliveGreen;
-            this.textBox1.Font = new System.Drawing.Font("Unispace", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.textBox1.Location = new System.Drawing.Point(61, 137);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(203, 98);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.Text = ">Phrase1   <\r\nPhrase2 \r\n";
-            // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.Transparent;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button1.Location = new System.Drawing.Point(66, 298);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(53, 45);
             this.button1.TabIndex = 1;
@@ -81,7 +66,7 @@ namespace RS_Trainer
             this.button2.BackColor = System.Drawing.Color.Transparent;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button2.Location = new System.Drawing.Point(129, 303);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(64, 44);
             this.button2.TabIndex = 2;
@@ -92,7 +77,7 @@ namespace RS_Trainer
             this.button3.BackColor = System.Drawing.Color.Transparent;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button3.Location = new System.Drawing.Point(202, 298);
-            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(54, 45);
             this.button3.TabIndex = 3;
@@ -104,7 +89,7 @@ namespace RS_Trainer
             this.button4.BackColor = System.Drawing.Color.Transparent;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button4.Location = new System.Drawing.Point(63, 358);
-            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(60, 33);
             this.button4.TabIndex = 4;
@@ -116,7 +101,7 @@ namespace RS_Trainer
             this.button5.BackColor = System.Drawing.Color.Transparent;
             this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button5.Location = new System.Drawing.Point(131, 356);
-            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(59, 36);
             this.button5.TabIndex = 5;
@@ -128,7 +113,7 @@ namespace RS_Trainer
             this.button6.BackColor = System.Drawing.Color.Transparent;
             this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button6.Location = new System.Drawing.Point(198, 355);
-            this.button6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button6.Margin = new System.Windows.Forms.Padding(2);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(60, 36);
             this.button6.TabIndex = 6;
@@ -140,7 +125,7 @@ namespace RS_Trainer
             this.button7.BackColor = System.Drawing.Color.Transparent;
             this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button7.Location = new System.Drawing.Point(63, 400);
-            this.button7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button7.Margin = new System.Windows.Forms.Padding(2);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(60, 34);
             this.button7.TabIndex = 7;
@@ -152,7 +137,7 @@ namespace RS_Trainer
             this.button8.BackColor = System.Drawing.Color.Transparent;
             this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button8.Location = new System.Drawing.Point(130, 400);
-            this.button8.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button8.Margin = new System.Windows.Forms.Padding(2);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(61, 34);
             this.button8.TabIndex = 8;
@@ -164,7 +149,7 @@ namespace RS_Trainer
             this.button9.BackColor = System.Drawing.Color.Transparent;
             this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button9.Location = new System.Drawing.Point(198, 400);
-            this.button9.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button9.Margin = new System.Windows.Forms.Padding(2);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(60, 34);
             this.button9.TabIndex = 9;
@@ -176,7 +161,7 @@ namespace RS_Trainer
             this.button10.BackColor = System.Drawing.Color.Transparent;
             this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button10.Location = new System.Drawing.Point(63, 444);
-            this.button10.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button10.Margin = new System.Windows.Forms.Padding(2);
             this.button10.Name = "button10";
             this.button10.Size = new System.Drawing.Size(60, 37);
             this.button10.TabIndex = 10;
@@ -188,7 +173,7 @@ namespace RS_Trainer
             this.button11.BackColor = System.Drawing.Color.Transparent;
             this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button11.Location = new System.Drawing.Point(131, 444);
-            this.button11.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button11.Margin = new System.Windows.Forms.Padding(2);
             this.button11.Name = "button11";
             this.button11.Size = new System.Drawing.Size(59, 37);
             this.button11.TabIndex = 11;
@@ -200,7 +185,7 @@ namespace RS_Trainer
             this.button12.BackColor = System.Drawing.Color.Transparent;
             this.button12.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button12.Location = new System.Drawing.Point(198, 444);
-            this.button12.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button12.Margin = new System.Windows.Forms.Padding(2);
             this.button12.Name = "button12";
             this.button12.Size = new System.Drawing.Size(58, 37);
             this.button12.TabIndex = 12;
@@ -212,7 +197,7 @@ namespace RS_Trainer
             this.button13.BackColor = System.Drawing.Color.Transparent;
             this.button13.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button13.Location = new System.Drawing.Point(63, 487);
-            this.button13.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button13.Margin = new System.Windows.Forms.Padding(2);
             this.button13.Name = "button13";
             this.button13.Size = new System.Drawing.Size(60, 38);
             this.button13.TabIndex = 13;
@@ -224,7 +209,7 @@ namespace RS_Trainer
             this.button14.BackColor = System.Drawing.Color.Transparent;
             this.button14.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button14.Location = new System.Drawing.Point(131, 487);
-            this.button14.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button14.Margin = new System.Windows.Forms.Padding(2);
             this.button14.Name = "button14";
             this.button14.Size = new System.Drawing.Size(59, 38);
             this.button14.TabIndex = 14;
@@ -236,7 +221,7 @@ namespace RS_Trainer
             this.button15.BackColor = System.Drawing.Color.Transparent;
             this.button15.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button15.Location = new System.Drawing.Point(198, 487);
-            this.button15.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button15.Margin = new System.Windows.Forms.Padding(2);
             this.button15.Name = "button15";
             this.button15.Size = new System.Drawing.Size(60, 38);
             this.button15.TabIndex = 15;
@@ -265,21 +250,17 @@ namespace RS_Trainer
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
             this.DoubleBuffered = true;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.TransparencyKey = System.Drawing.Color.LimeGreen;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
         private Button button1;
         private Button button2;
         private Button button3;
--- RS-Trainer/Form1.cs
+++ RS-Trainer/Form1.cs
@@ -20,20 +20,8 @@ namespace RS_Trainer
         int inputPtr = 0;
         String currentPassword;
 
-        void setTextStage1()
-        {
-            var lines = textBox1.Lines;
-            lines[0] = ">" + variants[currentStage][currentVariant] + "<";
-            lines[1] = variants[currentStage][(currentVariant + 1) % variants[currentStage].Count];
-            textBox1.Clear();
-            textBox1.Lines = lines;
-        }
-
-        void setText(String txt)
-        {
-            textBox1.Clear();
-            textBox1.Text = txt;
-        }
+        List<Label> line1;
+        List<Label> line2;
 
         void nextInputPassword(Char nxt)
         {
@@ -48,9 +36,41 @@ namespace RS_Trainer
             textBox1.Lines = new string[] { line1, txt.ToString() };
         }
 
+        public void InitializeLines()
+        {
+            line1 = new List<Label>(12);
+            line2 = new List<Label>(12);
+
+            for (int i = 0; i < 12; ++i)
+            {
+                line1.Add(new Label());
+                line2.Add(new Label());
+
+                line1[i].Location = new System.Drawing.Point(76 + i * 14, 151);
+                line1[i].Name = "line1[" + i + "]";
+                line1[i].Size = new System.Drawing.Size(13, 23);
+                line1[i].BackColor = Color.Silver;
+                line1[i].BorderStyle = BorderStyle.FixedSingle;
+
+                this.Controls.Add(line1[i]);
+
+                line2[i].Location = new System.Drawing.Point(76 + i * 14, 180);
+                line2[i].Name = "line2[" + i + "]";
+                line2[i].Size = new System.Drawing.Size(13, 23);
+                line2[i].BackColor = Color.Silver;
+                line2[i].BorderStyle = BorderStyle.FixedSingle;
+
+                this.Controls.Add(line2[i]);
+            }
+        }
+
+
+
         public Form1()
         {
+            InitializeLines();
             InitializeComponent();
+            /*
             variants = new List<List<String>>();
             variants.Add(new List<String>());
             variants[0].Add("Работа");
@@ -58,6 +78,7 @@ namespace RS_Trainer
             variants[0].Add("Тест");
             variants.Add(new List<String>());
             setTextStage1();
+            */
         }
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
@@ -72,17 +93,19 @@ namespace RS_Trainer
 
         private void button3_Click(object sender, EventArgs e)
         {
+            /*
             if (currentStage == 0)
             {
                 currentVariant += 1;
                 currentVariant %= variants[0].Count;
                 setTextStage1();
             }
-            
+            */
         }
 
         private void button13_Click(object sender, EventArgs e)
         {
+            /*
             if (currentStage == 0)
             {
                 setText("Доступ закрыт");
@@ -118,6 +141,7 @@ namespace RS_Trainer
                     textBox1.Lines = lines;
                 }
             }
+            */
         }
 
         private void button4_Click(object sender, EventArgs e)
@@ -198,6 +222,11 @@ namespace RS_Trainer
             {
                 nextInputPassword('0');
             }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
