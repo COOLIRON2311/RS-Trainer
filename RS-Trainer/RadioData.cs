7b9c6014b9b84082d10ab849502ca953ae1a97f3
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2022-03-22 17:44:41 +0300

    Added RadioData parser class

---
+++ RS-Trainer/RadioData.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.IO;
+
+namespace RS_Trainer
+{
+    public class RadioData
+    {
+        public string Adress { get; private set; }
+        public string[] Key { get; private set; }
+        public string CheckSum { get; private set; }
+        public string[] Freq { get; private set; }
+        private static string Slice(string s)
+        {
+            int i = s.IndexOf('!');
+            if (i >= 0)
+                return s.Substring(0, i).Trim();
+            return s;
+        }
+        public RadioData(string file)
+        {
+            string[] lines = File.ReadAllLines(file).Select(x => Slice(x)).Where(x => x.Length > 0).ToArray();
+            Adress = lines[0];
+            Key = lines.Skip(1).Take(8).ToArray();
+            CheckSum = lines[9];
+            Freq = lines.Skip(10).Take(6).ToArray();
+        }
+    }
+}
+
