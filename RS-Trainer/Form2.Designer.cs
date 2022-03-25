f6b4f67da430df15b81056dbbe03b9cd2420e46e
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2022-03-25 21:55:48 +0300

    Added basic components

--- RS-Trainer/Form2.Designer.cs
+++ RS-Trainer/Form2.Designer.cs
@@ -29,52 +29,108 @@ namespace RS_Trainer
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.RSBox = new System.Windows.Forms.PictureBox();
+            this.MTGBox = new System.Windows.Forms.PictureBox();
+            this.mtgb = new System.Windows.Forms.Button();
+            this.antb = new System.Windows.Forms.Button();
+            this.callb = new System.Windows.Forms.Button();
+            this.tangentb = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.RSBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MTGBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // RSBox
             // 
-            this.pictureBox1.Image = global::RS_Trainer.Properties.Resources.S1;
-            this.pictureBox1.Location = new System.Drawing.Point(370, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(880, 649);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.RSBox.Image = global::RS_Trainer.Properties.Resources.S1;
+            this.RSBox.Location = new System.Drawing.Point(370, 12);
+            this.RSBox.Name = "RSBox";
+            this.RSBox.Size = new System.Drawing.Size(880, 649);
+            this.RSBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.RSBox.TabIndex = 0;
+            this.RSBox.TabStop = false;
             // 
-            // pictureBox2
+            // MTGBox
             // 
-            this.pictureBox2.Image = global::RS_Trainer.Properties.Resources.PUMTG;
-            this.pictureBox2.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(352, 649);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
+            this.MTGBox.Image = global::RS_Trainer.Properties.Resources.PUMTG;
+            this.MTGBox.Location = new System.Drawing.Point(12, 12);
+            this.MTGBox.Name = "MTGBox";
+            this.MTGBox.Size = new System.Drawing.Size(352, 649);
+            this.MTGBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.MTGBox.TabIndex = 1;
+            this.MTGBox.TabStop = false;
+            // 
+            // mtgb
+            // 
+            this.mtgb.AllowDrop = true;
+            this.mtgb.BackColor = System.Drawing.Color.Transparent;
+            this.mtgb.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.mtgb.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.mtgb.Location = new System.Drawing.Point(502, 238);
+            this.mtgb.Name = "mtgb";
+            this.mtgb.Size = new System.Drawing.Size(43, 43);
+            this.mtgb.TabIndex = 2;
+            this.mtgb.UseVisualStyleBackColor = false;
+            this.mtgb.Click += new System.EventHandler(this.mtg_Click);
+            // 
+            // antb
+            // 
+            this.antb.BackColor = System.Drawing.Color.Transparent;
+            this.antb.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.antb.Location = new System.Drawing.Point(1108, 397);
+            this.antb.Name = "antb";
+            this.antb.Size = new System.Drawing.Size(32, 29);
+            this.antb.TabIndex = 3;
+            this.antb.UseVisualStyleBackColor = false;
+            this.antb.Click += new System.EventHandler(this.ant_Click);
+            // 
+            // callb
+            // 
+            this.callb.BackColor = System.Drawing.Color.Transparent;
+            this.callb.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.callb.Location = new System.Drawing.Point(105, 177);
+            this.callb.Name = "callb";
+            this.callb.Size = new System.Drawing.Size(10, 31);
+            this.callb.TabIndex = 4;
+            this.callb.UseVisualStyleBackColor = false;
+            // 
+            // tangentb
+            // 
+            this.tangentb.BackColor = System.Drawing.Color.Transparent;
+            this.tangentb.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.tangentb.Location = new System.Drawing.Point(236, 198);
+            this.tangentb.Name = "tangentb";
+            this.tangentb.Size = new System.Drawing.Size(23, 197);
+            this.tangentb.TabIndex = 5;
+            this.tangentb.UseVisualStyleBackColor = false;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1262, 673);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.tangentb);
+            this.Controls.Add(this.callb);
+            this.Controls.Add(this.antb);
+            this.Controls.Add(this.mtgb);
+            this.Controls.Add(this.MTGBox);
+            this.Controls.Add(this.RSBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form2";
             this.Text = "Form2";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RSBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MTGBox)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox RSBox;
+        private System.Windows.Forms.PictureBox MTGBox;
+        private System.Windows.Forms.Button mtgb;
+        private System.Windows.Forms.Button antb;
+        private System.Windows.Forms.Button callb;
+        private System.Windows.Forms.Button tangentb;
     }
 }
--- RS-Trainer/Form2.cs
+++ RS-Trainer/Form2.cs
@@ -12,9 +12,54 @@ namespace RS_Trainer
 {
     public partial class Form2 : Form
     {
+        public static bool MTG_Attached {get; private set;}
+        public static bool ANT_Attached { get; private set; }
         public Form2()
         {
             InitializeComponent();
+            //mtg.BackColor = Color.Transparent;
+            //mtg.ForeColor = Color.Transparent;
+            MTG_Attached = false;
+        }
+
+        private void mtg_Click(object sender, EventArgs e)
+        {
+            if (MTG_Attached)
+            {
+                if (ANT_Attached)
+                    RSBox.Image = Properties.Resources.S2;
+                else
+                    RSBox.Image = Properties.Resources.S1;
+                MTG_Attached = false;
+            }
+            else
+            {
+                if (ANT_Attached)
+                    RSBox.Image = Properties.Resources.S3;
+                else
+                     RSBox.Image = Properties.Resources.S4;
+                MTG_Attached = true;
+            }
+        }
+
+        private void ant_Click(object sender, EventArgs e)
+        {
+            if (ANT_Attached)
+            {
+                if (MTG_Attached)
+                    RSBox.Image = Properties.Resources.S4;
+                else
+                    RSBox.Image = Properties.Resources.S1;
+                ANT_Attached = false;
+            }
+            else
+            {
+                if (MTG_Attached)
+                    RSBox.Image = Properties.Resources.S3;
+                else
+                    RSBox.Image = Properties.Resources.S2;
+                ANT_Attached = true;
+            }
         }
     }
 }
